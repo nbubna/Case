cb859ad468e1e3ae476ea999d7818671360176f3
Nathan Bubna <nathan@esha.com>
2013-06-10 14:04:08 -0700

    use bower.json as main definition

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     // Metadata.
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('bower.json'),
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
