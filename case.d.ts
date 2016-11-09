299ff61e67a6b0aa2ccf1c55810700d723273a6d
Evan Shortiss <evanshortiss@gmail.com>
2016-11-09 16:04:58 -0600

    add typings for ts support

---
+++ case.d.ts
@@ -0,0 +1,37 @@
+
+declare module 'case' {
+  namespace Case {
+    function upper(str: string, fill: string) : string;
+    function upper(str: string) : string;
+
+    function lower(str: string, fill: string) : string;
+    function lower(str: string) : string;
+
+    function capital(str: string, fill: string) : string;
+    function capital(str: string) : string;
+
+    function snake(str: string) : string;
+
+    function pascal(str: string) : string;
+
+    function camel(str: string) : string;
+
+    function kebab(str: string) : string;
+
+    function constant(str: string) : string;
+
+    function title(str: string) : string;
+
+    function sentence(str: string, names: Array<string>) : string;
+
+    function of(str: string) : string;
+
+    function flip(str: string) : string
+
+    function random(str: string) : string;
+
+    function type(type: string, converter: (str: string) => string) : string;
+  }
+
+  export = Case;
+}
