b80050db5bb19c361b79aa23572617d534935f2f
Nathan Bubna <nathan@esha.com>
2017-03-22 13:46:43 -0700

    1.5.1

--- dist/Case.js
+++ dist/Case.js
@@ -1,4 +1,4 @@
-/*! Case - v1.5.0 - 2017-03-09
+/*! Case - v1.5.1 - 2017-03-22
 * Copyright (c) 2017 Nathan Bubna; Licensed MIT, GPL */
 (function() {
     "use strict";
@@ -46,15 +46,18 @@
             return s.replace(re.apostrophe, '');
         },
         fill: function(s, fill, deapostrophe) {
-            if (s) {
-                s = fill == null ? s : s.replace(re.fill, function(m, next) {
+            if (fill != null) {
+                s = s.replace(re.fill, function(m, next) {
                     return next ? fill + next : '';
                 });
-                return deapostrophe ? _.deapostrophe(s) : s;
             }
+            if (deapostrophe) {
+                s = _.deapostrophe(s);
+            }
+            return s;
         },
         prep: function(s, fill, pascal, upper) {
-            if (!s){ return s || ''; }
+            s = s == null ? '' : s + '';// force to string
             if (!upper && re.upper.test(s)) {
                 s = _.low.call(s);
             }
